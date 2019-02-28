a96ed8ea44e1cd951c7e5a8a30e777f128552e86
yangbing <yangbing@ipaynow.cn>
2019-02-28 19:37:02 +0800

    optimize code

--- springdemo/src/main/java/com/lyoyang/controller/IndexController.java
+++ springdemo/src/main/java/com/lyoyang/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.lyoyang.controller;
 
 
 import com.lyoyang.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
